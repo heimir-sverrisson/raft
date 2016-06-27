de75e7466d37dae6458898d2a18a9035181d1d8a
Heimir Sverrisson <heimir.sverrisson@gmail.com>
2016-06-26 22:30:45 -0600

    UDP sockets working in a single multithreaded program

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # raft
 Yet another implementation of the RAFT distributed concensus algorithm.
-The implementation is done in C++ and is mostly for my pleasure working on low level things 
-that include networking, distributed systems and hard algorithms.
+The implementation is done in C++ and is mostly for my pleasure working on low level things that include networking, distributed systems and hard algorithms.
+
+##Status
+The following things are starting to work:
+
+* UDP client and server sockets
+* Multi-threading
--- src/raft.cc
+++ src/raft.cc
@@ -1,31 +1,42 @@
 #include <iostream>
 #include <UDPSocket.h>
 #include <cstring>
+#include <thread>
 
 using namespace std;
 
-int main(int argc, char *arg[]) {
-    if(argc > 1){
-        if(strcmp(arg[1],"send") == 0){
-          try{
-            UDPSocket s = UDPSocket("localhost", "2001", 
-                                  UDPSocket::SocketType::client);
-            s.send(arg[2]);
-          } catch(const char *msg) {
-            cout << "Error message: " << msg << endl;
-          }
-        } else {
-            string str;
-            UDPSocket s = UDPSocket("localhost", "2001", 
-                                  UDPSocket::SocketType::server);
-            while(1){
-              s.receive(str, 100);
-              cout << str << endl;
-            }
-        }
+bool done = false;
 
-    } else {
-        cerr << "Need at least one argument receive or send" << endl;
+void receive_msg(void){
+  string str;
+  UDPSocket s = UDPSocket("localhost", "2001", 
+                        UDPSocket::SocketType::server);
+  while(!done){
+    s.receive(str, 100);
+    cout << str << endl;
+  }
+}
+
+void send_msg(void){
+  string msgs[] = { "Halló", "Bless", "Hvað", "Svo" };
+
+  try{
+    UDPSocket sock = UDPSocket("localhost", "2001", 
+                          UDPSocket::SocketType::client);
+    for(string s : msgs){
+      sock.send(s);
+      this_thread::sleep_for(chrono::milliseconds(1000));
     }
-    return 0;
+    done = true;
+    sock.send("");
+  } catch(const char *msg) {
+    cout << "Error message: " << msg << endl;
+  }
+}
+
+int main(int argc, char *arg[]) {
+  thread rcv(&receive_msg);
+  thread snd(&send_msg);
+  snd.join();
+  rcv.join();
 }
