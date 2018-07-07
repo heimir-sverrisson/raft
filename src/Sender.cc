0e9f7e4960577ce5a6c7f5439df9dbfff0eb62c4
Heimir Thor Sverrisson <heimir.sverrisson@gmail.com>
2018-07-07 12:56:27 -0600

    Adding logging to Sender

--- src/Sender.cc
+++ src/Sender.cc
@@ -5,58 +5,56 @@
 #include <Receiver.h>
 #include <AppendResponse.h>
 
-void
-Sender::sendVoteResponse(ServerState& ss, int candidateId, int vote){
-  char sep = Config::messageSeparator;
-  BOOST_LOG_TRIVIAL(info) << "Sender: Sending VoteResponse";
-  HostEntry h = ss.getHostList().getHostById(candidateId);
-  UDPSocket sock(h.getHost(), h.getService(), clientSocket);
-  VoteResponse vr(ss.getMyId(), ss.getTerm(), vote);
-  string s = to_string(MessageType::voteResponse) + sep + vr.to_string();
-  sock.send(s);
-  sock.close();
-}
-
-void
-Sender::sendAppendResponse(ServerState& ss, int leaderId, int success){
-  char sep = Config::messageSeparator;
-  BOOST_LOG_TRIVIAL(info) << "Sender: Sending AppendResponse";
-  HostEntry h = ss.getHostList().getHostById(leaderId);
-  UDPSocket sock(h.getHost(), h.getService(), clientSocket);
-  AppendResponse ar(ss.getMyId(), ss.getTerm(), success);
-  string s = to_string(MessageType::appendResponse) + sep + ar.to_string();
-  sock.send(s);
-  sock.close();
-}
-
-void
-Sender::sendRequestVote(ServerState& ss){
-  int myId = ss.getMyId();
-  char sep = Config::messageSeparator;
-  RequestVote rv(ss.getTerm(), myId, ss.getLastLogIndex(), ss.getLastLogTerm());
-  for(HostEntry& h : ss.getHostList().getHosts()){
-    if(h.getNodeId() == myId) // Never send to myself
-      continue;
+void Sender::sendVoteResponse(ServerState& ss, int candidateId, int vote) {
+    char sep = Config::messageSeparator;
+    BOOST_LOG_TRIVIAL(info) << "Sender: Sending VoteResponse";
+    HostEntry h = ss.getHostList().getHostById(candidateId);
     UDPSocket sock(h.getHost(), h.getService(), clientSocket);
-    string s = to_string(MessageType::requestVote) + sep + rv.to_string();
+    VoteResponse vr(ss.getMyId(), ss.getTerm(), vote);
+    string s = to_string(MessageType::voteResponse) + sep + vr.to_string();
     sock.send(s);
     sock.close();
-  }
-  BOOST_LOG_TRIVIAL(info) << "Sender: Sent RequestVote to all";
 }
 
-void
-Sender::sendAppendEntries(ServerState& ss){
-  char sep = Config::messageSeparator;
-  BOOST_LOG_TRIVIAL(info) << "Sender: Sending AppendEntries to all";
-  AppendEntries ae(ss.getTerm(), ss.getMyId(), 1, 2, 3 ); // TODO: correct params needed
-  string s = to_string(MessageType::appendEntries) + sep + ae.to_string();
-  for(HostEntry& h : ss.getHostList().getHosts()){
-    if(h.getNodeId() == ss.getMyId()) // Never send to myself
-      continue;
+void Sender::sendAppendResponse(ServerState& ss, int leaderId, int success) {
+    char sep = Config::messageSeparator;
+    BOOST_LOG_TRIVIAL(info) << "Sender: Sending AppendResponse";
+    HostEntry h = ss.getHostList().getHostById(leaderId);
     UDPSocket sock(h.getHost(), h.getService(), clientSocket);
+    AppendResponse ar(ss.getMyId(), ss.getTerm(), success);
+    string s = to_string(MessageType::appendResponse) + sep + ar.to_string();
     sock.send(s);
     sock.close();
-  }
 }
 
+void Sender::sendRequestVote(ServerState& ss) {
+    int myId = ss.getMyId();
+    char sep = Config::messageSeparator;
+    RequestVote rv(ss.getTerm(), myId, ss.getLastLogIndex(),
+                   ss.getLastLogTerm());
+    for (HostEntry& h : ss.getHostList().getHosts()) {
+        if (h.getNodeId() == myId)  // Never send to myself
+            continue;
+        UDPSocket sock(h.getHost(), h.getService(), clientSocket);
+        string s = to_string(MessageType::requestVote) + sep + rv.to_string();
+        sock.send(s);
+        sock.close();
+    }
+    BOOST_LOG_TRIVIAL(info) << "Sender: Sent RequestVote to all";
+}
+
+void Sender::sendAppendEntries(ServerState& ss) {
+    char sep = Config::messageSeparator;
+    BOOST_LOG_TRIVIAL(info) << "Sender: Sending AppendEntries to all";
+    AppendEntries ae(ss.getTerm(), ss.getMyId(), 1, 2,
+                     3);  // TODO: correct params needed
+    string s = to_string(MessageType::appendEntries) + sep + ae.to_string();
+    for (HostEntry& h : ss.getHostList().getHosts()) {
+        if (h.getNodeId() == ss.getMyId())  // Never send to myself
+            continue;
+        UDPSocket sock(h.getHost(), h.getService(), clientSocket);
+        sock.send(s);
+        sock.close();
+    }
+    BOOST_LOG_TRIVIAL(info) << "Sender: All AppendEntries sent";
+}
